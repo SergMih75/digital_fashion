c1f34cf01b02db2ebfb8da29a773d1c9eba6b481
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2024-05-10 00:31:05 +0300

    first edition

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const title = document.querySelectorAll('.title-start')
+
+title.forEach((title, index) => {
+	setTimeout(() => {
+		title.style.animation = 'titleMove'
+		title.style.animationDuration = 3500 + 'ms'
+		title.style.animationFillMode = 'forwards'
+	}, (index + 1) * 2000)
+})
+
+setTimeout(() => {
+	document.querySelector('.cookies').style.display = 'flex'
+}, 10000)
+
+const checkbox = document.getElementById('cookies')
+
+checkbox.addEventListener('change', () => {
+	if (checkbox.checked) {
+		console.log('Согласие получено')
+	}
+})
